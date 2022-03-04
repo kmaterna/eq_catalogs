d65aeb17abee118cb68e77d30930fd6786af7204
kmaterna <kathrynmaterna@gmail.com>
2022-03-03 20:34:53 -0800

    adding further configurability to pygmt plots

--- pygmt_plots.py
+++ pygmt_plots.py
@@ -3,7 +3,7 @@ import pygmt
 import datetime as dt
 from . import catalog_functions
 
-def simple_pygmt_map(mycat, filename, legendfile=None):
+def simple_pygmt_map(mycat, filename, legendfile=None, scalelength=1, cbar_interval=1.0, map_frame_int=0.05):
     """A basic pygmt plot for earthquake catalogs, color-coded by depth and size-coded by magnitude."""
     lons = [eq.lon for eq in mycat];
     lats = [eq.lat for eq in mycat];
@@ -16,9 +16,11 @@ def simple_pygmt_map(mycat, filename, legendfile=None):
     proj = "M7i"
     fig = pygmt.Figure()
     fig.coast(region=region, projection=proj, borders='2', shorelines='0.5p,black', water='lightblue',
-              resolution='h', frame="0.05", map_scale="jBR+c"+str(region[2])+"+o0.6/0.7+w1k");
+              resolution='h', frame=str(map_frame_int),
+              map_scale="jBR+c"+str(region[2])+"+o0.6/0.7+w"+str(scalelength)+"k");
     fig.plot(x=lons, y=lats, color=depths, size=mags, style='c', cmap="mycpt.cpt", pen="thin,black");  # earthquakes
-    fig.colorbar(position="jBr+w3.5i/0.2i+o2.5c/1.5c+h", cmap="mycpt.cpt", frame=["x" + str(1.0), "y+L\"km\""]);
+    fig.colorbar(position="jBr+w3.5i/0.2i+o3.5c/1.5c+h", cmap="mycpt.cpt",
+                 frame=["x" + str(cbar_interval), "y+L\"km\""]);
     starttime, endtime = catalog_functions.get_start_stop_time(mycat);
     startstr = dt.datetime.strftime(starttime, "%Y-%m-%d");
     endstr = dt.datetime.strftime(endtime, "%Y-%m-%d");
